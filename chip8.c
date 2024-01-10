95ed6b5e27e44bc9f2776da096fe9b7e8964d721
Arlie <Arlie4004@gmail.com>
2024-01-10 12:09:02 +1100

    allowed initialisation of chip8

--- chip8.c
+++ chip8.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "SDL.h"
+#include "fontset.h"
 
 #define MEMORY_SIZE 4096
 #define REGISTER_SIZE 16
-#define STACK_SIZE 48
+#define STACK_SIZE 16
+
 
 // SDL_t is a struct that contains the SDL window and renderer
 typedef struct {
@@ -21,19 +23,58 @@ typedef struct {
 
 // initialise chip8
 typedef struct {
-    int memory[MEMORY_SIZE];
-    int registers[REGISTER_SIZE];
-    int stack[STACK_SIZE];
-    int delay_timer;
-    int sound_timer;
-    int index;
-    int pc;
-
+    unsigned char opcode; // 2 byte opcode
+    unsigned char memory[MEMORY_SIZE]; // 4k memory
+    unsigned char V [REGISTER_SIZE]; // 16 registers
+    unsigned short stack[STACK_SIZE]; // A stack with 16 levels
+    unsigned char sp; // stack pointer
+    unsigned char gfx [64 * 32]; // graphics
+    unsigned char delay_timer;
+    unsigned char sound_timer;
+    unsigned short I; // index register
+    unsigned short pc; // program counter
+    unsigned char keypad[16]; // keypad
 } Chip8_t;
 
 
+void init_chip8(Chip8_t *chip8) {
+    chip8->opcode = 0; // reset opcode
+    chip8->pc = 0x200; // program counter starts at 0x200
+    chip8->I = 0; // reset index register
+    chip8->sp = 0; // reset stack pointer
 
-void init_chip8(DisplayConfig_t *displayConfig){
+//    clear display
+    for (int i = 0; i < 2048; i++) {
+        chip8->gfx[i] = 0;
+    }
+
+//    clear stack
+    for (int i = 0; i < 16; i++) {
+        chip8->stack[i] = 0;
+    }
+
+//    clear registers
+    for (int i = 0; i < 16; i++) {
+        chip8->V[i] = 0;
+    }
+
+//    clear memory
+    for (int i = 0; i < 4096; i++) {
+        chip8->memory[i] = 0;
+    }
+
+//    load fontset
+    for (int i = 0; i < 80; i++) {
+        chip8->memory[i] = fontset[i];
+    }
+
+//    reset timers
+    chip8->delay_timer = 0;
+    chip8->sound_timer = 0;
+
+}
+
+void init_display(DisplayConfig_t *displayConfig){
     displayConfig->window_height = 32; // Original chip8 height
     displayConfig->window_width = 64; // Original chip8 width
     displayConfig->window_scale = 20; // Scale the window by 10
@@ -100,13 +141,17 @@ int main (int argc, char **argv) {
 
     // initialise chip8
     DisplayConfig_t  displayConfig = {0};
-    init_chip8(&displayConfig);
+    init_display(&displayConfig);
 
     // initialise sdl
     SDL_t sdl = {0};
     if (!init_sdl(&sdl, &displayConfig)) {
         return EXIT_FAILURE;
     }
+
+    // initialise chip8
+    Chip8_t chip8 = {0};
+    init_chip8(&chip8);
 
     // main loop
     int running = 1;
